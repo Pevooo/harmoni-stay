300e3c42e09f86fdc420854192fdd6fa6c0cc744
JessicaAyman <jysykaaymnn@gmail.com>
2023-11-09 21:39:10 +0200

    jessica2

--- Models/Employee.cs
+++ Models/Employee.cs
@@ -7,7 +7,7 @@
         public double EmployeeSalary { get; set; }
 
         public double WorkingHours { get; set; }
-        public string Facility { get; set; }
+        
 
         public virtual Facility FacilityEmployee { get; set; }
     }
