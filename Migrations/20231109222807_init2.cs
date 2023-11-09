085aa5b9612d697dff22ddabb1212c82621229c1
Aya-Abdellatif <ayaabdllatif112@gmail.com>
2023-11-10 00:51:36 +0200

    Guest class Done

---
+++ Migrations/20231109222807_init2.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MainProject.Migrations
+{
+    /// <inheritdoc />
+    public partial class init2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guests_Rooms_RoomID",
+                table: "Guests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guests_RoomID",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "RoomID",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "Facility",
+                table: "Employees");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ChechIn",
+                table: "Guests",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "CheckOut",
+                table: "Guests",
+                type: "datetime",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.AddColumn<int>(
+                name: "GuestRoomRoomID",
+                table: "Guests",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "GustRoomType",
+                table: "Guests",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_GuestRoomRoomID",
+                table: "Guests",
+                column: "GuestRoomRoomID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guests_Rooms_GuestRoomRoomID",
+                table: "Guests",
+                column: "GuestRoomRoomID",
+                principalTable: "Rooms",
+                principalColumn: "RoomID",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Guests_Rooms_GuestRoomRoomID",
+                table: "Guests");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Guests_GuestRoomRoomID",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "ChechIn",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "CheckOut",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "GuestRoomRoomID",
+                table: "Guests");
+
+            migrationBuilder.DropColumn(
+                name: "GustRoomType",
+                table: "Guests");
+
+            migrationBuilder.AddColumn<int>(
+                name: "RoomID",
+                table: "Guests",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Facility",
+                table: "Employees",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Guests_RoomID",
+                table: "Guests",
+                column: "RoomID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Guests_Rooms_RoomID",
+                table: "Guests",
+                column: "RoomID",
+                principalTable: "Rooms",
+                principalColumn: "RoomID");
+        }
+    }
+}
--- Models/Guest.cs
+++ Models/Guest.cs
@@ -1,8 +1,16 @@
-﻿namespace MainProject
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MainProject
 {
     public class Guest
     {
         public int GuestID { get; set; }
         public string GuestName { get; set; }
+        public string GustRoomType { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime ChechIn { get; set; }
+        [Column(TypeName = "datetime")]
+        public DateTime CheckOut{ get; set; }
+        public virtual Room GuestRoom { get; set; }
     }
 }
