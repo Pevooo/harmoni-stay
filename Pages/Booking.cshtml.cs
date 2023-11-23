8094e1894ca782e9cb2daef6faebab4417e467c3
Pevooo <pavlysamuelatef@gmail.com>
2023-11-23 13:08:31 +0200

    Empty Booking Razor Pages

---
+++ Pages/Booking.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MainProject.Pages
+{
+    public class BookingModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
