29ecf50ba8cbf90af73e842a3c22ba08b15f1c67
Aya Abdellatif <106776143+Aya-Abdellatif@users.noreply.github.com>
2023-11-27 03:25:35 +0200

    Booking

---
+++ Pages/Booking.cshtml.cs
@@ -0,0 +1,72 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Http;
+using MainProject.Models;
+
+namespace MainProject.Pages
+{
+    public class BookingModel : PageModel
+    {
+        //[BindProperty]
+        //public Booking booking { get; set; }
+        public bool Error { get; set; }
+        public DateTime CheckIn { get; set;}
+        public DateTime CheckOut { get; set; }
+        public int GuestId { get; set; }
+        private readonly Context db;
+
+        public BookingModel()
+        {
+            this.db = db;
+        }
+
+        public void OnGet()
+        {
+            //booking = new Booking();
+            //date = DateOnly.FromDateTime(DateTime.Now);
+        }
+        public void OnPost()
+        {
+            try 
+            {
+                CheckIn = Convert.ToDateTime(Request.Form["checkin"]);
+                CheckOut = Convert.ToDateTime(Request.Form["checkout"]);
+                GuestId = Convert.ToInt32(Request.Form["guestId"]);
+
+            } 
+            catch 
+            {
+                Error = true;
+                return;
+            }
+
+            if(CheckIn >= CheckOut)
+            {
+                Error = true;
+                return;
+            }
+
+            
+            // Saving User info in Session and Globals
+           // HttpContext.Session.SetInt32("GuestId", (int)GuestId);
+            //Globals.UserId = UserId;
+            //Response.Redirect("/Index", false, true);
+             //return Page();
+        }
+
+        //private Booking reservation(DateTime checkin, DateTime checkout, int guestId, string roomType)
+        //{
+        //    var queryGuest = db.Bookings.Where(x => x.BookingGuest.GuestID == guestId);
+        //    // && account.RoomType == roomType);
+        //    if (queryGuest is null) //new guest
+        //    {
+                
+        //    }
+        //    else
+        //    {
+
+        //    }
+        //    return null;
+        //}
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,30 +1,57 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using MainProject.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.AspNetCore.Http;
 
 namespace MainProject.Pages
 {
     public class IndexModel : PageModel
     {
-        private readonly ILogger<IndexModel> _logger;
+        //[BindProperty]
+        //public Booking booking { get; set; }
+        public bool Error { get; set; }
+        public DateTime CheckIn { get; set; }
+        public DateTime CheckOut { get; set; }
+        public int GuestId { get; set; }
+        public string GuestName { get; set; }
+        public string GuestNationality {  get; set; }
+        public string GuestPhoneNumber { get; set; }
+        private readonly Context db;
+        //public IndexModel(ILogger<IndexModel> logger)
+        //{
+        //    _logger = logger;
+        //}
 
-        public int? UserId;
-
-        public static ISession Session { get; set; }
-        public IndexModel(ILogger<IndexModel> logger)
+        public void OnGet(int id)
         {
-            _logger = logger;
-        }
+            GuestId = id;
+            var queryGuest = db.Guests.Where(x => x.GuestID == GuestId).Select(x=>new{ x.GuestName,x.GuestNationality,x.GuestPhoneNumber});
+            if (queryGuest is not null)
+            {
+                foreach(var x in queryGuest)
+                {
+                    GuestName = x.GuestName;
+                    GuestNationality = x.GuestNationality;
+                    GuestPhoneNumber = x.GuestPhoneNumber;
+                }
+            }
+            //Session = HttpContext.Session;
+            //UserId = HttpContext.Session.GetInt32("userID");
 
-        public void OnGet()
-        {
-            Session = HttpContext.Session;
-            UserId = HttpContext.Session.GetInt32("userID");
         }
 
         public void OnPost()
         {
-            UserId = HttpContext.Session.GetInt32("userID");
+            // Updating the database
+				Guest? guest = db.Guests.SingleOrDefault(g => g.GuestID == GuestId);
+				
+				if (guest is null)
+				{
+					Error = true;
+					return;
+				}
+				
+				db.Guests.Add(new Guest() {  GuestID = GuestId, GuestName = GuestName, GuestNationality = GuestNationality, GuestPhoneNumber = GuestPhoneNumber });
+				db.SaveChanges();
+            //UserId = HttpContext.Session.GetInt32("userID");
         }
     }
 }
