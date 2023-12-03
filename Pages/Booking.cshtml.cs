85fc3a1f381e663bf52e9a808c1ffe77040ae6a4
Aya-Abdellatif <ayaabdllatif112@gmail.com>
2023-12-03 02:06:26 +0200

    lOGIN(front)&Booking

--- Pages/Booking.cshtml.cs
+++ Pages/Booking.cshtml.cs
@@ -13,6 +13,9 @@ namespace MainProject.Pages
         public DateTime CheckIn { get; set;}
         public DateTime CheckOut { get; set; }
         public int GuestId { get; set; }
+        public string? GuestName { get; set; }
+        public string? GuestNationality { get; set; }
+        public string? GuestPhoneNumber { get; set; }
         private readonly Context db;
 
         public BookingModel(Context db)
@@ -22,16 +25,18 @@ namespace MainProject.Pages
 
         public void OnGet()
         {
+           
             //booking = new Booking();
             //date = DateOnly.FromDateTime(DateTime.Now);
         }
+        
         public void OnPost()
         {
             try 
             {
-                CheckIn = Convert.ToDateTime(Request.Form["checkin"]);
-                CheckOut = Convert.ToDateTime(Request.Form["checkout"]);
-                GuestId = Convert.ToInt32(Request.Form["guestId"]);
+                this.CheckIn = Convert.ToDateTime(Request.Form["checkin"]);
+                this.CheckOut = Convert.ToDateTime(Request.Form["checkout"]);
+                this.GuestId = Convert.ToInt32(Request.Form["guestId"]);
 
             } 
             catch 
@@ -40,25 +45,37 @@ namespace MainProject.Pages
                 return;
             }
 
-            if(CheckIn >= CheckOut)
+            if (this.CheckOut <= this.CheckIn)
             {
                 Error = true;
                 return;
             }
+            //var RoomTypes
+            var queryGuest = db.Guests.Where(x => x.GuestID == this.GuestId).Select(x => new { x.GuestName, x.GuestNationality, x.GuestPhoneNumber });
+            if (queryGuest is not null)
+            {
+                foreach (var x in queryGuest)
+                {
+                    this.GuestName = x.GuestName;
+                    this.GuestNationality = x.GuestNationality;
+                    this.GuestPhoneNumber = x.GuestPhoneNumber;
+                }
+            }
+
+
+            // Updating the database
+            Guest? guest = db.Guests.SingleOrDefault(g => g.GuestID == this.GuestId);
+
+            if (guest is null)
+            {
+                db.Guests.Add(new Guest() { GuestID = this.GuestId, GuestName = this.GuestName, GuestNationality = this.GuestNationality, GuestPhoneNumber = GuestPhoneNumber });
+                db.SaveChanges();
+            }
 
             
-            // Saving User info in Session and Globals
-           // HttpContext.Session.SetInt32("GuestId", (int)GuestId);
-            //Globals.UserId = UserId;
-            //Response.Redirect("/Index", false, true);
-             //return Page();
         }
 
-        //private Booking reservation(DateTime checkin, DateTime checkout, int guestId, string roomType)
-        //{
-        //    var queryGuest = db.Bookings.Where(x => x.BookingGuest.GuestID == guestId);
-        //    // && account.RoomType == roomType);
-        //    if (queryGuest is null) //new guest
+        / is null) //new guest
         //    {
                 
         //    }
