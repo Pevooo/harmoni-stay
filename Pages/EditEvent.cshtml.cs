53b3863d3dc8b973ac6b6d42b6501836906b9ad6
JessicaAyman <jysykaaymnn@gmail.com>
2023-12-18 00:54:37 +0200

    jessica

--- Pages/EditEvent.cshtml.cs
+++ Pages/EditEvent.cshtml.cs
@@ -1,12 +1,67 @@
+using MainProject.Models;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System;
+using System.Security.Policy;
 
 namespace MainProject.Pages
 {
     public class EditEventModel : PageModel
     {
-        public void OnGet()
+
+        private readonly Context db;
+        public EditEventModel(Context db)
         {
+            this.db = db;
+
+        }
+        public string EventName { get; set; }
+
+        public string EventType { get; set; }
+        public string Id { get; set; }
+        public DateTime st { get; set; }
+        public DateTime end { get; set; }
+
+        public bool Error { get; set; }
+       public void OnGet(string id)
+        {
+            if (HttpContext.Session.GetString("UserId") is null)
+            {
+                Response.Redirect("/", false, true);
+            }
+            var eventToEdit = db.Events.FirstOrDefault(x => x.EventID == id);
+
+            EventName = eventToEdit.EventName;
+            Id = eventToEdit.EventID;
+
+            st = eventToEdit.EventStart;
+            end = eventToEdit.EventEnd;
+
+        }
+        public void OnPost(string id)
+        {
+            try
+            {
+                var eventToEdit = db.Events.FirstOrDefault(x => x.EventID == id);
+
+                EventName = Request.Form["EventName"];
+                st = DateTime.Parse(Request.Form["startDate"]);
+                end = DateTime.Parse(Request.Form["endDate"]);
+
+                eventToEdit.EventName = EventName;
+                eventToEdit.EventStart = st;
+                eventToEdit.EventEnd = end;
+
+
+                db.SaveChanges();
+                Response.Redirect("/Events", false, true);
+            }
+            catch
+            {
+                Error = true;
+                return;
+
+            }
 
         }
     }
