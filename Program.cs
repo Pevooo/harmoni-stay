c61b60eaedc89c94b8dbc62b2cc94bebc18cfadc
Aya-Abdellatif <ayaabdllatif112@gmail.com>
2023-12-21 20:46:52 +0200

    set database as hosted database

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ namespace MainProject
             builder.Services.AddRazorPages();
             builder.Services.AddDistributedMemoryCache();
             builder.Services.AddSession(options => options.IdleTimeout = TimeSpan.FromDays(30));
-            builder.Services.AddDbContext<Models.Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Local Db")));
+            builder.Services.AddDbContext<Models.Context>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("Hosted Db")));
 
             var app = builder.Build();
 
