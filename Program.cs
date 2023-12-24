17a132d27de4248de0f10c529e132da3d6b6f9ee
Pavly <pavlysamuelatef@gmail.com>
2023-12-24 17:01:16 +0200

    Exceptions Handled by Error

--- Program.cs
+++ Program.cs
@@ -18,9 +18,9 @@ namespace MainProject
             var app = builder.Build();
 
             // Configure the HTTP request pipeline.
+            app.UseExceptionHandler("/Error");
             if (!app.Environment.IsDevelopment())
             {
-                app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
